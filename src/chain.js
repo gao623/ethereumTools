1cab9fe537a4f9232499042525019c9afc8619f8
linaly <gao.gaoshuang@gmail.com>
2021-12-24 17:05:15 +0800

    add getPastLogs and tx decoder

--- src/chain.js
+++ src/chain.js
@@ -1,5 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3Contract = require('web3-eth-contract');
+const web3EthAbi = require("web3-eth-abi");
 const Web3 = require('web3');
 const pu = require('promisefy-util');
 
@@ -10,6 +11,32 @@ class Chain {
             providerOrUrl: nodeUrl,
         });
         this.web3 = new Web3(this.provider);
+    }
+
+    static calcEventSignature(eventName, abi) {
+        let info = abi.filter(function (json) {
+        return (json.type === 'event' && json.name === eventName);
+        });
+        if (info.length !== 1) {
+        return null;
+        }
+        return web3EthAbi.encodeEventSignature(info[0]);
+    }
+
+    static hexStrip0x(str) {
+        return str.replace(/^0x/, '');
+      }
+
+    static hexWith0x(hexStr){
+        if(0 > hexStr.indexOf('0x')){
+            return `0x${hexStr}`;
+        }
+        return hexStr;
+    }
+
+    static getEventHash(eventName, contractAbi) {
+        let eventHash = Chain.calcEventSignature(eventName, contractAbi);
+        return eventHash ? Chain.hexWith0x(eventHash) : eventHash;
     }
 
     getContract(abi, contractAddr) {
@@ -31,6 +58,21 @@ class Chain {
         }
         return await pu.promisefy(sc.methods[func]().call, [], sc.methods);
     }
+
+    async getPastLogs(address, topics, options) {
+        let { abi, eventName, fromBlock, toBlock } = options;
+        if (eventName) {
+            topics[0] = Chain.getEventHash(eventName, abi);
+        }
+
+        let filter = {
+            fromBlock: fromBlock || 0,
+            toBlock: toBlock || "latest",
+            topics: topics,
+            address: address
+        };
+        return await this.web3.eth.getPastLogs(filter);
+    }
 }
 
-module.exports = Chain;
+exports.Chain = Chain;
